d7a8e670044933f57df23813a1e62ae7f81f239f
murkjenga <jeleonguwm@gmail.com>
2023-12-12 18:40:12 -0600

    Update

--- Bot.ts
+++ Bot.ts
@@ -1,19 +1,14 @@
-require('dotenv').config(); 
-const { Client, Collection, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Client({
-    intents: [
-        // May need to add more intents
-        GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-    ],
-});
-client.commands = new Collection(); 
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers] });
 
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -23,9 +18,9 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
+            console.log(command.data.name, command)
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -34,7 +29,6 @@ for (const folder of commandFolders) {
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
@@ -54,8 +48,9 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-client.once('ready', () => {
-    console.log('Bot is ready!');
-}); 
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
-client.login(process.env.DISCORD_TOKEN); 
+client.login(token);
+
