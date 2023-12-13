b7568c0fe808193f693378347010bb1fc809978d
murkjenga <jeleonguwm@gmail.com>
2023-12-12 22:55:57 -0600

    Update

---
+++ commands/custom/pong.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pong')
+        .setDescription('Replies with piss off!'),
+    async execute(interaction) { 
+        const embedColor = parseInt('0099ff', 16); 
+        const embed = {
+            color: embedColor,
+            title: 'Pong!',
+            description: 'Piss off!',
+        };
+ 
+        await interaction.reply({ embeds: [embed], ephemeral: false }); 
+        console.log(interaction);
+    },
+};
