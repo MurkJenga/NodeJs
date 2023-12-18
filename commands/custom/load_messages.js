22dc60d168779c39a6e33097e6cc7448b3ecdf4f
murkjenga <jeleonguwm@gmail.com>
2023-12-17 22:53:13 -0600

    Update

--- commands/custom/load_messages.js
+++ commands/custom/load_messages.js
@@ -17,19 +17,59 @@ module.exports = {
             description: 'Access Denied'
         }
         if (interaction.user.id == '553337834090659899') {
-            
-            // Loop through all channels in the guild
-            const channelsList = interaction.guild.channels.cache.map((channel) => {
-                return `Channel ID: ${channel.id}, Channel Name: ${channel.name}, Type: ${channel.type}`;
-            });
+            try {
+                let totalMessages = 0
+                // Loop through all channels in the guild
+                for (const channel of interaction.guild.channels.cache.values()) { 
+                    let lastMessageId
 
-            // Join the channel information into a string
-            const channelsInfo = channelsList.join('\n');
-            console.log(channelsInfo)
-            
-            await interaction.reply({ embeds: [embed_granted], ephemeral: false }); 
+                    // Only loop through text channels
+                    if (channel.type === 0) {
+                        console.log(channel.name, channel.type, channel.id)
+                        try {
+                            const options = { limit: 100, before: lastMessageId };
+                            const messages = await channel.messages.fetch(options);
+
+                            if (messages.size > 0) {
+                                messages.forEach((message) => {
+                                    console.log(`Message ID: ${message.id}, Content: ${message.content}`);
+                                    const fetchedMessage = message.channel.messages.fetch(message.id);
+
+                                    if (message.reactions.cache.size > 0) {
+                                        console.log("Reactions:");
+                                        message.reactions.cache.forEach((reaction) => {
+                                            
+                                            console.log(`  Emoji: ${reaction.emoji.name}, Count: ${reaction.count}, Users: ${reaction.users}`)
+                                               
+                                        });
+                                    } 
+                                    Object.keys(fetchedMessage).forEach((prop)=> console.log(prop))
+                                    //console.log(`  Emoji: ${fetchedMessage}`) 
+
+                                    totalMessages += 1
+                                });
+
+                                // Update the lastMessageId for the next iteration
+                                lastMessageId = messages.lastKey();
+                                
+                            } else {
+                                // No more messages
+                                break;
+                            }
+                        } catch (error) {
+                            console.error('Error fetching messages:', error);
+                            break;
+                        } 
+                    }
+                }
+                await interaction.reply({ embeds: [embed_granted], ephemeral: false });
+                console.log(totalMessages)
+            } catch (error) {
+                console.error('Error iterating through channels:', error);
+                await interaction.reply({ embeds: [embed_denied], ephemeral: false });
+            } 
         } else {
-            await interaction.reply({ embeds: [embed_denied], ephemeral: false }); 
-        }    
+            await interaction.reply({ embeds: [embed_denied], ephemeral: false });
+        }
     },
-}; 
+};
