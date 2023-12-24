d9e8a5f80c4e16362750df9cd1b03e0e52d7efca
murkjenga <jeleonguwm@gmail.com>
2023-12-24 01:08:19 -0600

    update

--- commands/custom/help.js
+++ commands/custom/help.js
@@ -7,16 +7,14 @@ module.exports = {
         .setDescription('Returns the slash commands'),
     async execute(interaction) { 
         try {
-           
-          for (const key in interaction) {
-            if (interaction.hasOwnProperty(key)) {
-              console.log(`${key}: ${interaction[key]}`);
-            }
-          }
-          console.log('Global Slash Commands:');
+
+          const explain = `Below are the new updates made as of 12/24 \n
+                           - ChatGPT command enhancements, more reliable \n
+                           - Reformatted some of the responses such as plex requests and total\n
+                           - Support for Jakes plex bot`
+          await interaction.reply({ embeds: [createdEmbed('ffffff', `New Updates!`, explain)], ephemeral: false })
         } catch (error) {
           console.error('Error fetching global slash commands:', error);
         }
-          await interaction.reply({ embeds: [createdEmbed('FF0000', 'Command')], ephemeral: false });
       }
     }
