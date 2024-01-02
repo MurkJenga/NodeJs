9d1f060bd20ec8172b24cae7d339ddb02f4119dd
murkjenga <jeleonguwm@gmail.com>
2024-01-01 21:10:02 -0600

    update

--- commands/custom/help.js
+++ commands/custom/help.js
@@ -17,8 +17,10 @@ module.exports = {
                            NEW COMMANDS: \n
                            - "whogave": Returns list of users and the times they gave an emoji to the specified user \n
                            - "whorecieved": Returns list of users and the times they gave an emoji to the specified user \n
-                           - "topmessage": Returns the message that has the most emojis on it for a given user`
-                           
+                           - "topmessage": Returns the message that has the most emojis on it for a given user \n
+                           - "last30": Shows stats from the last 30 days
+                           `
+
           await interaction.reply({ embeds: [createdEmbed('ffffff', `New Updates!`, explain)], ephemeral: false })
         } catch (error) {
           console.error('Error fetching global slash commands:', error);
